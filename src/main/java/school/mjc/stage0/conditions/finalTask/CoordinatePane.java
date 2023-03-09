e394a988e616b61518b7b739629cba7c9913d924
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-09 18:15:34 +0300

    Update CoordinatePane.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/CoordinatePane.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class CoordinatePane {
     public void printQuadrant(int x, int y) {
+           if (x == 0 && y == 0)
+            System.out.print("zero\n");
+        else if (x == 0 || y == 0)
+            System.out.print("Coordinates belong to two quarters at the same time");
+        else if (x >= 0 && y >= 0)
+            System.out.print("first\n");
+        else if (x <= 0 && y >= 0)
+            System.out.print("second\n");
+        else if (x <= 0 && y <= 0)
+            System.out.print("third\n");
+        else if (x >= 0 && y <= 0)
+            System.out.print("fourth\n");
     }
 }
