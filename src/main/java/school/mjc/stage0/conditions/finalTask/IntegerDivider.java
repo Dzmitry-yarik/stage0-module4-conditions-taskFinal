c03adf4e3b9282488411fb7a7c819224ef11618e
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-09 18:16:19 +0300

    Update IntegerDivider.java

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+                int result;
+        int res;
+        if (divider == 0) {
+            System.out.println("division by zero");
+        } else {
+            result = dividend / divider;
+            res = result * divider;
+            if (res == dividend)
+                System.out.println("can be divided completely");
+            else
+                System.out.println("cannot be divided completely");
+        }
     }
 }
